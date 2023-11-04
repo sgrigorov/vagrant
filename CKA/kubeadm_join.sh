eca6a540e857b4144ce00b35945280ad8281012d
Stan Grigorov <s_grigorov@yahoo.com>
2023-11-04 11:55:56 -0600

    message

--- CKA/kubeadm_join.sh
+++ CKA/kubeadm_join.sh
@@ -1,1 +1,1 @@
-kubeadm join 192.168.56.21:6443 --token fay8pe.838cqdddd65z33cg --discovery-token-ca-cert-hash sha256:46817e1d54913720e1290c47bacfdf707bc0dd03ef9e36890ed086338334c0de 
+kubeadm join 192.168.56.21:6443 --token ibnz8d.2usyxhltorp1t6wc --discovery-token-ca-cert-hash sha256:c0c8ba30f17fae81687c4166f8f781e91c786abd116ed4d7c4d03c9176baa4a7 
