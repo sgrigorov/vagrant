0513745c406ae8a9cef1bd44b12a770129cdda74
Stan <s_grigorov@yahoo.com>
2023-12-12 23:38:41 -0700

    Update git_vagrant.sh

--- git_vagrant.sh
+++ git_vagrant.sh
@@ -1,4 +1,11 @@
-git pull git@gitlab.com:sgrigorov/vagrant.git
-sed -i '/	url = git@gitlab.com:sgrigorov\/vagrant.git/a \	url = git@github.com:sgrigorov\/vagrant.git' ~/scripts/vagrant/.git/config
-git config commit.template ~/scripts/vagrant/.gitmessage
-echo " My commit  message" > ~/scripts/vagrant/.gitmessage
+REPOSITORY="vagrant"
+GITHUB="url = git@github.com:sgrigorov/$REPOSITORY.git"
+GITLAB="url = git@gitlab.com:sgrigorov/$REPOSITORY.git"
+
+FILE='.git/config'
+
+grep -qF -- "$GITLAB" "$FILE" || sed -i "/remote \"origin\"/a \	$GITLAB" "$FILE"
+grep -qF -- "$GITHUB" "$FILE" || sed -i "/remote \"origin\"/a \	$GITHUB" "$FILE"
+
+git config commit.template ~/scripts/terraform/.gitmessage
+echo " My commit  message" > ~/scripts/terraform/.gitmessage
