789d92bdbb0a88548d57bbe45398004614253334
Stan Grigorov <user.name@gmail.com>
2023-12-13 21:57:24 -0700

    My commit  message

--- CKA.new/setup_vagrant.sh
+++ CKA.new/setup_vagrant.sh
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 vagrant plugin install vagrant-disksize
-=======
-vagrant plugin install vagrant-disksize
->>>>>>> 754994f08e9e2391bfbfc51e4d3e5a5f9f4c033a
--- git_vagrant.sh
+++ git_vagrant.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 REPOSITORY="vagrant"
 GITHUB="url = git@github.com:sgrigorov/$REPOSITORY.git"
 GITLAB="url = git@gitlab.com:sgrigorov/$REPOSITORY.git"
@@ -10,9 +9,3 @@ grep -qF -- "$GITHUB" "$FILE" || sed -i "/remote \"origin\"/a \	$GITHUB" "$FILE"
 
 git config commit.template ~/scripts/terraform/.gitmessage
 echo " My commit  message" > ~/scripts/terraform/.gitmessage
-=======
-git pull git@gitlab.com:sgrigorov/vagrant.git
-sed -i '/	url = git@gitlab.com:sgrigorov\/vagrant.git/a \	url = git@github.com:sgrigorov\/vagrant.git' ~/scripts/vagrant/.git/config
-git config commit.template ~/scripts/vagrant/.gitmessage
-echo " My commit  message" > ~/scripts/vagrant/.gitmessage
->>>>>>> 754994f08e9e2391bfbfc51e4d3e5a5f9f4c033a
